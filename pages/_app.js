b2d9c31e68d1fa2b27a16e9bcd9931700752ed79
unknown <sadanandkr9686@gmail.com>
2022-02-03 10:45:33 +0530

    no changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <> 
+    <Head>
+    <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+
+  <script
+  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script>
+
+<script
+  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+/>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
+ 
+  }
 
 export default MyApp
